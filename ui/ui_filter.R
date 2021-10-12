b0cf4e12b73a6f00a73a0e00e0fa56cb6f537757
ShyGuyPy <luke.vawter1@gmail.com>
2021-10-12 16:39:48 -0400

    changed strucutre of selectizeInput

--- ui/ui_filter.R
+++ ui/ui_filter.R
@@ -3,11 +3,15 @@ fluidRow(style = "font-size:60%",
   #heading 1
   # column(width = 1.5, p("Primary Filter")),
   # box(width =4,
-      # column(width = 3 , p("Primary Filter"),style='text-align:center; border:3px solid #3c8dbc;'),
-  column(width = 2, uiOutput("filter_col_1")),
+      column(width = 4 , p("Primary Filter"), style='text-align:center; border:5px solid #3c8dbc;',#),
+  #column(width = 2, 
+  uiOutput("filter_col_1"),
+  #),
 
-  column(width = 2, uiOutput("filter_select_1"),style='margin-bottom:30px;border-right:5px solid #3c8dbc;'), ##padding: 5px;'),#),
+  #column(width = 2, 
+  uiOutput("filter_select_1"),#,style='margin-bottom:30px;border-right:5px solid #3c8dbc;'), ##padding: 5px;'),#),
   # ),#end box
+      ),#end column
   
   # br(),
   # conditionalPanel(condition = 'input.filter_col_1 != "none"',
@@ -15,20 +19,28 @@ fluidRow(style = "font-size:60%",
   #heading 2
   # column(width = 4, p("Secondary Filter")),                 
   # box(width =4,
-      # column(width = 3, p("Secondary Filter"),style='text-align:center; border:3px solid #3c8dbc;'),
-                   column(width = 2, uiOutput("filter_col_2")),
-                   column(width = 2, uiOutput("filter_select_2"),style='margin-bottom:30px;border-right:5px solid #3c8dbc;'), ##padding: 5px;'),#),
+      column(width = 4, p("Secondary Filter"),style='text-align:center; border:5px solid #3c8dbc;',#),
+                   #column(width = 2, 
+             uiOutput("filter_col_2"),
+             #),
+                   #column(width = 2, 
+             uiOutput("filter_select_2"),#,style='margin-bottom:30px;border-right:5px solid #3c8dbc;'), ##padding: 5px;'),#),
   # ),#end box
+      ),#end column
   
   # br(),
   # conditionalPanel(condition = 'input.filter_col_2 != "none"',
   
   #heading 3
   # box(width =4,
-  # column(width = 4, p("Tertiary Filter"),style='text-align:center; border:3px solid #3c8dbc;'),
-                   column(width = 2, uiOutput("filter_col_3")),
-                   column(width = 2, uiOutput("filter_select_3")),
+  column(width = 4, p("Tertiary Filter"),style='text-align:center; border:5px solid #3c8dbc;',#),
+                   #column(width = 2, 
+         uiOutput("filter_col_3"),
+         #),
+                   #column(width = 2, 
+         uiOutput("filter_select_3"),#),
   # ),#end box
+  ),#end column
   
   # ), #end box12
   
@@ -39,7 +51,7 @@ fluidRow(style = "font-size:60%",
          actionButton("reset",
                       "Reset Filter",
                       icon = icon("undo"),
-                      style = "width:100%; text-align: center;")
+                      style = "width:100%; text-align: center; margin-top: 7px;")
   ),
   br(),
   hr()#,
